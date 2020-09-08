9d9f1703d111c8b5b5d911a1e80a52466e4ef414
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-08 12:12:58 -0500

    add form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,20 @@ class UsersController < ApplicationController
     def new 
     end
     def create 
+        @user = Ha
+
+        @user = 
+        @user 
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
     end
+
+    private 
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
