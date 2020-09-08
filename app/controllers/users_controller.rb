d7be69b73ff1a4f3ea63d4c3aabd561f300d96bc
mateomh <mmojicah.84@gmail.com>
2020-09-08 14:53:33 -0500

    Change the form to use form and tags helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
     #redirect_to "http://google.com"
   end
   def create 
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #@user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   private 
-      def user_params
-          params.require(:user).permit(:username, :email, :password)
-      end
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
 end
