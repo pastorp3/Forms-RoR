d76560bcc62a0f3f1103c504bdfedb3a40fd4d07
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-08 15:51:10 -0500

    add edit and update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
   def new 
     @user = User.new
 
@@ -13,8 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+
+    if @user.update(user_params)
+        redirect_to new_user_path
+    else
+        render 'edit'
+    end
+  end
+
   private 
   def user_params
       params.require(:user).permit(:username, :email, :password)
+  end
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit]
+  resources :users do
+    put "edit", action: :edit
+  end
 end
