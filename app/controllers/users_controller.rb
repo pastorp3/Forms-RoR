04883380322f332869c1a9526357e6cfd31a0a68
mateomh <mmojicah.84@gmail.com>
2020-09-08 12:26:09 -0500

    Group all the inputs in the user hash in the users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
 class UsersController < ApplicationController
-    def new 
-    end
-    def create 
-        @user = Ha
+  def new 
+  end
+  def create 
+    #@user = User.new(params[:user])
+    p params[:user]
+    # if @user.save
+    #     redirect_to new_user_path
+    # else
+    #     render :new
+    # end
+  end
 
-        @user = 
-        @user 
-
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
-    end
-
-    private 
-        def user_params
-            params.require(:user).permit(:username, :email, :password)
-        end
+  private 
+      def user_params
+          params.require(:user).permit(:username, :email, :password)
+      end
 end
